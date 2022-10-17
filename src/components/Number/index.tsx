a60631eefaac2946dc3d51732e9472667710884d
Gabriel de Jesus Silva <gabriel.jesilva@gmail.com>
2022-10-17 16:56:54 -0300

    monday afternoon

--- src/components/Number/index.tsx
+++ src/components/Number/index.tsx
@@ -1,35 +1,60 @@
+import React, { useState } from "react";
 import { Container, Section, Inner_section, Span, Input, Options } from "./styles";
 
 
 
 export default function Number() {
+
+    const [initialNumber, setInitialNumber] = useState(0)
+    const [lastNumber, setLastNumber] = useState(0)
+
+
+    function randomNumber() {
+        return Math.floor(Math.random() * (lastNumber - initialNumber + 1)) + initialNumber
+
+    }
+
+    function handleOnChangeInitial(e: React.FormEvent<HTMLInputElement>) {
+        setInitialNumber(parseInt(e.currentTarget.value))
+    }
+
+    function handleOnChangeLast(e: React.FormEvent<HTMLInputElement>) {
+        setLastNumber(parseInt(e.currentTarget.value))
+    }
+
+
     return (
+
         <Container>
             <h1>Gere o seu número</h1>
 
             <Section>
 
+
                 <Options>
-                    
+
                     <Inner_section>
                         <Span>Número Inicial</Span>
-                        <Input type="number" placeholder="Ex: 2" />
+
+                        <Input type="number" placeholder="Ex: 2" onChange={handleOnChangeInitial} />
                     </Inner_section>
+
 
                     <Inner_section>
                         <Span>Número Final</Span>
-                        <Input type="number" placeholder="Ex: 9" />
+                        <Input type="number" placeholder="Ex: 9" onChange={handleOnChangeLast} />
                     </Inner_section>
 
                 </Options>
 
                 <Inner_section>
                     <Span>Resultado:</Span>
-                    <Input type="text"  />
+                    <Input type="number" value={randomNumber()} />
                 </Inner_section>
 
             </Section>
 
         </Container>
+
     )
 }
--- src/components/Password/index.tsx
+++ src/components/Password/index.tsx
@@ -1,21 +1,65 @@
 import { Container, Section, Options, Inner_section, Span, Check, Input, CheckBox } from "./styles";
+import React, { useState } from "react";
 
 export default function Password() {
+
+
+    const [length, setLength] = useState(0)
+    const [checkUpperCase, setcheckUpperCase] = useState(false)
+    const [checkNumber, setcheckNumber] = useState(false)
+    const [CheckSymbols, setCheckSymbols] = useState(false)
+
+    function generatePassword() {
+
+        const numbers: string[] = ["0", "1", "2", "3", "4", '5', '6', '7', "8", "9"]
+        const symbols: string[] = ["!", "@", "#", "$", "%", "&", "*"]
+        const letters = Array.from(Array(26)).map((_, i) => i + 97)
+        const lowerletters = letters.map((letter) => String.fromCharCode(letter))
+        const upperletters = lowerletters.map((letter) => (letter.toUpperCase()))
+
+        const password: string[] = []
+
+        password.concat(lowerletters)
+
+        if (checkUpperCase) {
+            password.concat(upperletters)
+        }
+
+        if (checkNumber) {
+            password.concat(numbers)
+        }
+
+        if (CheckSymbols) {
+            password.concat(symbols)
+        }
+
+
+        for (let i = 0; i <= length; i++) {
+
+
+        }
+
+    
+    }
+
     return (
         <Container>
             <h1>Gere sua senha</h1>
 
             <Options>
                 <Section>
+                    <>
+                    {console.log(generatePassword())}
+                    </>
 
                     <Inner_section>
                         <Span>Número de caracteres da senha </Span>
-                        <Input type="number" placeholder="Ex: 8" />
+                        <Input type="number" placeholder="Ex: 8" onChange={(e: React.FormEvent<HTMLInputElement>) => setLength(parseInt(e.currentTarget.value))} />
                     </Inner_section>
 
                     <Inner_section>
                         <Span>Resultado:</Span>
-                        <Input type="text" />
+                        <Input type="text"  />
 
                     </Inner_section>
 
@@ -24,17 +68,17 @@ export default function Password() {
                 <Section>
 
                     <Check>
-                        <CheckBox type="checkbox" />
+                        <CheckBox type="checkbox" onChange={(e: React.FormEvent<HTMLInputElement>) => checkUpperCase ? setcheckUpperCase(false) : setcheckUpperCase(true)} />
                         <Span>Letra Maiúscula</Span>
                     </Check>
 
                     <Check>
-                        <CheckBox type="checkbox" />
+                        <CheckBox type="checkbox" onChange={(e: React.FormEvent<HTMLInputElement>) => checkNumber ? setcheckNumber(false) : setcheckNumber(true)} />
                         <Span>Números</Span>
                     </Check>
 
                     <Check>
-                        <CheckBox type="checkbox" />
+                        <CheckBox type="checkbox" onChange={(e: React.FormEvent<HTMLInputElement>) => CheckSymbols ? setCheckSymbols(false) : setCheckSymbols(true)} />
                         <Span>Símbolos</Span>
                     </Check>
 
