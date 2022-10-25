ad614a6d414afabc757f618d2924cba4bd21489d
Gabriel de Jesus Silva <gabriel.jesilva@gmail.com>
2022-10-25 15:09:48 -0300

    tuesday afternoon

--- src/components/Password/styles.ts
+++ src/components/Password/styles.ts
@@ -1,47 +1,59 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
 display: flex;
 flex-direction: column;
 align-items: center; 
-margin: 2% 3% 1% 3%;
+margin: 1em 3% 2em 3%;
 border: 3px solid ${props => props.theme.colors.text};
 border-radius: 10px;
 color: ${props => props.theme.colors.text};
 background-color: ${props => props.theme.colors.primary};
 box-shadow: 0 0 15px 1px black;
-
-
 `
+
 export const Section = styled.div`
 display: flex;
 flex-direction: column;
 padding: 2%;
-
 `
 
 export const Options = styled.div`
 display: flex;
 justify-content: space-around;
 `
+
 export const Inner_section = styled.div`
 display: flex;
 flex-direction: column;
-margin: 3% 0  ;
-width: 25em; // fazer alterar depois para fazer responsivo
+margin: 3% 0;
+width: 25em; 
+
+@media (max-width: 720px) {
+  width: 12em;
+}
 `
 
 export const Span = styled.span`
 font-size: 1.7em;
+
+@media (max-width: 720px) {
+  font-size: 1.5em;
+}
 `
+
 export const Check = styled.div`
 margin: 6% 0% 5% 0%;
 width: 14em;
 display: flex;
-`
-export const Input = styled.input`
 
+@media (max-width: 720px) {
+  width: 12.3em;
+  margin: 3% 0% 23% 0%;
+}
+`
+
+export const Input = styled.input`
 background-color: ${props => props.theme.colors.primary_background};
 border-color: black;
 border-radius: 6px;
@@ -72,8 +84,8 @@ export const CheckBox = styled.input.attrs({ type: "checkbox" })`
 width: 8%;
 margin-right: 2%;
 margin-top: 1.5%;
-
 `
+
 export const Buttons = styled.div`
 display: flex;
 justify-content: space-around;
